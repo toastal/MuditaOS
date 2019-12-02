5f277bde2e6fd2eec81c367868d2a0674e77084e
Mateusz Grzywacz <mateusz.grzywacz@mudita.com>
2019-12-02 17:22:54 +0100

    [EGD-1635] initial files

---
+++ module-apps/application-calculator/windows/CalculatorMainWindow.hpp
@@ -0,0 +1,42 @@
+/*
+ * @file CallLogMainWindow.hpp
+ * @author Aleksander Rudnik (aleksander.rudnik@mudita.com)
+ * @date 19.09.2019
+ * @brief Application Call Log Main Window
+ * @copyright Copyright (C) 2019 mudita.com
+ * @details
+ */
+#pragma once
+
+#include <string>
+#include <functional>
+
+#include "AppWindow.hpp"
+#include "gui/widgets/Label.hpp"
+#include "gui/widgets/Image.hpp"
+#include "gui/widgets/Window.hpp"
+#include "gui/widgets/BottomBar.hpp"
+#include "gui/widgets/TopBar.hpp"
+#include "gui/widgets/ListView.hpp"
+
+#include "../CalllogModel.hpp"
+
+namespace gui {
+
+class CalculatorMainWindow: public AppWindow {
+	gui::ListView* list = nullptr;
+public:
+	CalculatorMainWindow( app::Application* app );
+	virtual ~CalculatorMainWindow();
+
+	//virtual methods
+	bool onInput( const InputEvent& inputEvent ) override;
+	void onBeforeShow( ShowMode mode, SwitchData* data ) override;
+
+	void rebuild() override;
+	void buildInterface() override;
+	void destroyInterface() override;
+	bool onDatabaseMessage( sys::Message* msg ) override;
+};
+
+} /* namespace gui */
