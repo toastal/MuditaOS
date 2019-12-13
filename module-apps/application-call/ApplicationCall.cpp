93e9547638374951ecaa2dbbb6acefbcc82d07cb
Mateusz Grzywacz <mateusz.grzywacz@mudita.com>
2019-12-13 13:03:16 +0100

    [EGD-2368] | AppCall adapted to AppTimers #2

--- module-apps/application-call/ApplicationCall.cpp
+++ module-apps/application-call/ApplicationCall.cpp
@@ -30,13 +30,17 @@ ApplicationCall::ApplicationCall(std::string name, std::string parent, bool star
     timerCall = CreateAppTimer(1000, true, [=]() {timerCallCallback();} );
 }
 
-    timerCallId = registerTimer(1000, true, [=]() {
-        // Invoked when timer ticked, 3 seconds after end call event if user didn't press back button earlier.
-        ++callDuration;
-        auto it = windows.find("CallWindow");
-        if (currentWindow == it->second)
-        {
-            gui::CallWindow *callWindow = reinterpret_cast<gui::CallWindow *>(currentWindow);
+ApplicationCall::~ApplicationCall() {
+}
+
+void ApplicationCall::timerCallCallback()
+{
+    // Invoked when timer ticked, 3 seconds after end call event if user didn't press back button earlier.
+    ++callDuration;
+    auto it = windows.find("CallWindow");
+    if (currentWindow == it->second)
+    {
+        gui::CallWindow *callWindow = reinterpret_cast<gui::CallWindow *>(currentWindow);
 
         if (callWindow->getState() == gui::CallWindow::State::CALL_IN_PROGRESS)
         {
