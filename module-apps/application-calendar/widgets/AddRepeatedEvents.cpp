f9da3311a52c71aa3a867311a688687d40605065
marek303 <marek303@poczta.onet.pl>
2020-09-02 20:17:27 +0200

    git commit -m "Some repeated events reworked"

--- module-apps/application-calendar/widgets/AddRepeatedEvents.cpp
+++ module-apps/application-calendar/widgets/AddRepeatedEvents.cpp
@@ -5,60 +5,62 @@
 #include <module-db/Interface/EventsRecord.hpp>
 #include <module-services/service-db/api/DBServiceAPI.hpp>
 #include <module-db/queries/calendar/QueryEventsAdd.hpp>
+#include <module-utils/date/include/date/dateCommon.h>
 
 void AddRepeatedEvents::addDaily(std::shared_ptr<EventsRecord> event)
 {
-    //    auto record = event.get();
-    //    for (uint32_t i = 1; i <= 7; i++) {
-    ////        record->date_from = record->date_from + 10000 * i;
-    ////        record->date_from = record->date_from + 10000 * i;
-    ////        DBServiceAPI::GetQuery(
-    ////            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
-    //    }
+    auto record = event.get();
+    for (uint32_t i = 1; i <= 7; i++) {
+        record->date_from += date::days{1};
+        record->date_from += date::days{1};
+        DBServiceAPI::GetQuery(
+            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
+    }
 }
 
 void AddRepeatedEvents::addWeekly(std::shared_ptr<EventsRecord> event)
 {
-    //    auto record = event.get();
-    //    for (uint32_t i = 1; i <= 4; i++) {
-    ////        record->date_from = record->date_from + 70000 * i;
-    ////        record->date_from = record->date_from + 70000 * i;
-    ////        DBServiceAPI::GetQuery(
-    ////            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
-    //    }
+    auto record = event.get();
+    for (uint32_t i = 1; i <= 4; i++) {
+        record->date_from += date::days{7};
+        record->date_from += date::days{7};
+        DBServiceAPI::GetQuery(
+            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
+    }
 }
 void AddRepeatedEvents::addTwoWeeks(std::shared_ptr<EventsRecord> event)
 {
-    //    auto record = event.get();
-    //    for (uint32_t i = 1; i <= 4; i++) {
-    ////        record->date_from = record->date_from + 140000 * i;
-    ////        record->date_from = record->date_from + 140000 * i;
-    ////        DBServiceAPI::GetQuery(
-    ////            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
-    //    }
+    auto record = event.get();
+    for (uint32_t i = 1; i <= 4; i++) {
+        record->date_from += date::days{14};
+        record->date_from += date::days{14};
+        DBServiceAPI::GetQuery(
+            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
+    }
 }
 void AddRepeatedEvents::addMonth(std::shared_ptr<EventsRecord> event)
 {
-    //    auto record = event.get();
-    //    for (uint32_t i = 1; i <= 12; i++) {
-    ////        record->date_from = record->date_from + 1000000 * i;
-    ////        record->date_from = record->date_from + 1000000 * i;
-    ////        DBServiceAPI::GetQuery(
-    ////            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
-    //    }
+    auto record = event.get();
+    for (uint32_t i = 1; i <= 4; i++) {
+        record->date_from += date::months{1};
+        record->date_from += date::months{1};
+        DBServiceAPI::GetQuery(
+            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
+    }
 }
 void AddRepeatedEvents::addYear(std::shared_ptr<EventsRecord> event)
 {
-    //    auto record = event.get();
-    //    for (uint32_t i = 1; i <= 5; i++) {
-    //        record->date_from = record->date_from + 100000000 * i;
-    //        record->date_from = record->date_from + 100000000 * i;
-    //        DBServiceAPI::GetQuery(
-    //            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
-    //   }
+    auto record = event.get();
+    for (uint32_t i = 1; i <= 4; i++) {
+        record->date_from += date::years{1};
+        record->date_from += date::years{1};
+        DBServiceAPI::GetQuery(
+            application, db::Interface::Name::Events, std::make_unique<db::query::events::Add>(*record));
+    }
 }
 void AddRepeatedEvents::addCustom(std::shared_ptr<EventsRecord> event)
 {
+
     //    auto record = event.get();
     //    for (uint32_t i = 1; i <= 7; i++) {
     //        record->date_from = record->date_from + 10000 * i;
