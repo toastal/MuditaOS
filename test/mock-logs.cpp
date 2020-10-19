52696be94a856601035dfaed3416eaa74cf54433
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-10-19 09:08:01 +0200

    [EGD-4075] Thread local storage fix
    Fixing thread local storage when running on the
    Linux platform in the test mode

--- test/mock-logs.cpp
+++ test/mock-logs.cpp
@@ -1,7 +1,7 @@
 #include <log/log.hpp>
 #include <cstdarg>
-
-void log_Log(logger_level level, const char *file, int line, const char *function, const char *fmt, ...)
+__attribute__((weak)) void log_Log(
+    logger_level level, const char *file, int line, const char *function, const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
