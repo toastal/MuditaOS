6e867f65a0e85a91b5fcb2b4c9fe5a91df93f536
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2020-12-01 00:10:01 +0100

    empty buffer in case there is no data to ret

--- module-audio/Audio/Stream.hpp
+++ module-audio/Audio/Stream.hpp
@@ -11,15 +11,18 @@ namespace audio
       public:
         struct Span
         {
-            std::uint8_t *data;
-            std::size_t dataSize;
+            std::uint8_t *data   = nullptr;
+            std::size_t dataSize = 0;
         };
 
         static constexpr auto defaultBufferingSize = 4U;
 
         Stream(std::size_t blockSize, unsigned int bufferingSize = defaultBufferingSize)
-            : _blockSize(blockSize), _buffer(std::make_unique<std::uint8_t[]>(blockSize * bufferingSize))
-        {}
+            : _blockSize(blockSize), _buffer(std::make_unique<std::uint8_t[]>(blockSize * bufferingSize)),
+              _emptyBuffer(std::make_unique<std::uint8_t[]>(blockSize))
+        {
+            std::fill(_emptyBuffer.get(), _emptyBuffer.get() + blockSize, 0);
+        }
 
         bool push(void *data, std::size_t dataSize);
         bool push(const Span &span)
@@ -45,6 +48,7 @@ namespace audio
       private:
         std::size_t _blockSize = 0;
         std::unique_ptr<uint8_t[]> _buffer;
+        std::unique_ptr<uint8_t[]> _emptyBuffer;
     };
 
 } // namespace audio
