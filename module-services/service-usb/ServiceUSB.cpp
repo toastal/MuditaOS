eb2ae7994dde241bd270091c49b933e3d8b0ecff
Marek Niepieklo <marek.niepieklo@mudita.com>
2020-12-04 21:15:59 +0100

    [EGD-4681] Add: create USB service
    Added a skeleton of service-usb.

---
+++ module-services/service-usb/ServiceUSB.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "Constants.hpp"
+#include "service-usb/ServiceUSB.hpp"
+
+#include <module-sys/Service/Bus.hpp>
+
+#include <Service/Service.hpp>
+#include <Service/Message.hpp>
+
+#include <log/log.hpp>
+
+ServiceUSB::ServiceUSB() : sys::Service(service::name::service_usb)
+{
+    LOG_INFO("[ServiceUSB] Initializing");
+}
+
+ServiceUSB::~ServiceUSB()
+{
+    LOG_INFO("[ServiceUSB] Cleaning resources");
+}
+
+sys::ReturnCodes ServiceUSB::InitHandler()
+{
+    LOG_ERROR("ServiceUSB experimental!");
+
+    return sys::ReturnCodes::Success;
+}
+
+sys::ReturnCodes ServiceUSB::DeinitHandler()
+{
+
+    return sys::ReturnCodes::Success;
+}
+
+sys::MessagePointer ServiceUSB::DataReceivedHandler(sys::DataMessage *msg, sys::ResponseMessage *resp)
+{
+
+    return std::make_shared<sys::ResponseMessage>();
+}
+
+sys::ReturnCodes ServiceUSB::SwitchPowerModeHandler(const sys::ServicePowerMode mode)
+{
+    LOG_ERROR("TODO ServiceUSB");
+    return sys::ReturnCodes::Success;
+}
---
+++ module-services/service-usb/service-usb/ServiceUSB.hpp
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <Service/Service.hpp>
+#include <Utils.hpp>
+
+#include <functional>
+
+class ServiceUSB : public sys::Service
+{
+  public:
+    ServiceUSB();
+
+    ~ServiceUSB();
+
+    sys::MessagePointer DataReceivedHandler(sys::DataMessage *msg, sys::ResponseMessage *resp) override;
+
+    sys::ReturnCodes InitHandler() override;
+
+    sys::ReturnCodes DeinitHandler() override;
+
+    sys::ReturnCodes SwitchPowerModeHandler(const sys::ServicePowerMode mode) override;
+
+  private:
+    // std::unique_ptr<UsbWorker> worker;
+};
