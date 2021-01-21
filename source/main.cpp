c31a0f92ff3e7de254652c07d72ed4385b781440
Hubert Chrzaniuk <hubert.chrzaniuk@mudita.com>
2021-01-21 19:33:30 +0100

    Speed test, not to be merged

--- source/main.cpp
+++ source/main.cpp
@@ -51,6 +51,8 @@
 #include <memory>
 #include <vector>
 
+#define TICKS_TO_MSEC(tick) ((uint32_t)((uint64_t)(tick)*1000uL / (uint64_t)configTICK_RATE_HZ))
+#include <fcntl.h>
 
 int main()
 {
@@ -69,6 +71,8 @@ int main()
     auto sysmgr = std::make_shared<sys::SystemManager>(5000);
     purefs::subsystem::vfs_handle_t vfs;
 
+    ///////////////////////////////////////////////
+
     sysmgr->StartSystem([sysmgr, &vfs]() {
         /// force initialization of PhonenumberUtil because of its stack usage
         /// otherwise we would end up with an init race and PhonenumberUtil could
@@ -76,10 +80,143 @@ int main()
         i18n::phonenumbers::PhoneNumberUtil::GetInstance();
         vfs     = purefs::subsystem::initialize();
         int err = purefs::subsystem::mount_defaults();
+
         if (err) {
             LOG_FATAL("VFS subystem fatal error %i", err);
             std::abort();
         }
+
+        //////////// FILESYSTEM ONE-SHOT TEST //////////////
+        {
+            // SETTINGS
+            auto testSizeInSectors = 10000;     // size of the test buffer in MMC sectors
+            auto firstTestSector   = 2048 * 10; // first sector to write to
+            auto deviceName        = "emmc0";
+            auto testFileName      = "testfile.crap";
+            auto lfsTestFileName   = std::string(purefs::dir::getUserDiskPath() / testFileName);
+            auto fatTestFileName   = std::string(purefs::dir::getRootDiskPath() / testFileName);
+
+            auto diskManager = std::get<0>(vfs);
+            auto fs          = std::get<1>(vfs);
+
+            const auto handle     = diskManager->device_handle(deviceName);
+            const auto sectorSize = diskManager->get_info(handle, purefs::blkdev::info_type::sector_size);
+
+            auto writeSize = sectorSize * testSizeInSectors;
+            LOG_DEBUG("Test size %ld MB", (long)writeSize / 1024 / 1024);
+
+            // std::unique_ptr<char[]> testPatternBuf(new char[writeSize]);
+            std::unique_ptr<char[]> workBuf(new char[writeSize]);
+
+            enum class BufOp
+            {
+                Fill,
+                Compare,
+                Clear
+            };
+            auto bufferOperation = [](BufOp op, char *buf, size_t writeSize) {
+                if (op == BufOp::Clear) {
+                    std::memset(buf, 0, writeSize);
+                    return true;
+                }
+
+                // attempts to use non-trivial pattern to fill
+                char writePiece  = 0;
+                auto noiseOffset = 1;
+                for (size_t idx = 0; idx < writeSize; idx++) {
+                    if (writePiece == 0xFF) {
+                        for (int nidx = 0; (nidx < noiseOffset) && (idx < writeSize); nidx++) {
+                            if (op == BufOp::Fill) {
+                                buf[idx] = noiseOffset & 0xFF;
+                            }
+                            else if (buf[idx] != (noiseOffset & 0xFF)) {
+                                return false;
+                            }
+                            idx++;
+                        }
+                        noiseOffset++;
+                    }
+                    if (op == BufOp::Fill) {
+                        buf[idx] = writePiece;
+                    }
+                    else if (buf[idx] != writePiece) {
+                        return false;
+                    }
+                    writePiece++;
+                };
+                return true;
+            };
+
+            auto vfsTest = [&fs, &workBuf, &writeSize, &bufferOperation](std::string system, std::string path) {
+                // VFS TEST littlefs
+                auto file = fs->open(path, O_CREAT | O_RDWR, S_IRWXU);
+                if (file <= 0) {
+                    LOG_ERROR("Failed creating test file %s.", path.c_str());
+                    return false;
+                }
+
+                bufferOperation(BufOp::Fill, workBuf.get(), writeSize);
+                auto start = xTaskGetTickCount();
+                fs->write(file, workBuf.get(), writeSize);
+                auto tickCount           = xTaskGetTickCount() - start;
+                auto speedInBytesPerMsec = writeSize / TICKS_TO_MSEC(tickCount);
+                LOG_DEBUG("VFAT %s write speed %ld B/ms", system.c_str(), (long)(speedInBytesPerMsec));
+                fs->close(file);
+
+                file = fs->open(path, O_RDWR, S_IRWXU);
+                if (file <= 0) {
+                    LOG_ERROR("Failed creating test file %s.", path.c_str());
+                    return false;
+                }
+
+                bufferOperation(BufOp::Clear, workBuf.get(), writeSize);
+                start         = xTaskGetTickCount();
+                auto sizeRead = fs->read(file, workBuf.get(), writeSize);
+                tickCount     = xTaskGetTickCount() - start;
+                if (sizeRead != writeSize) {
+                    LOG_ERROR("Error reading %s.", system.c_str());
+                }
+                speedInBytesPerMsec = writeSize / TICKS_TO_MSEC(tickCount);
+                LOG_DEBUG("VFAT %s read speed %ld B/ms", system.c_str(), (long)(speedInBytesPerMsec));
+
+                if (bufferOperation(BufOp::Compare, workBuf.get(), writeSize) == false) {
+                    LOG_ERROR("Bad write on %s.", system.c_str());
+                    return true;
+                }
+
+                fs->close(file);
+                return true;
+            };
+
+            LOG_DEBUG("Testing LFS file %s", std::string(lfsTestFileName).c_str());
+            vfsTest("LFS", std::string(lfsTestFileName));
+
+            LOG_DEBUG("Testing FAT file %s", std::string(fatTestFileName).c_str());
+            vfsTest("FAT", std::string(fatTestFileName));
+
+            bufferOperation(BufOp::Fill, workBuf.get(), writeSize);
+
+            auto start = xTaskGetTickCount();
+            diskManager->write(handle, workBuf.get(), firstTestSector, testSizeInSectors);
+            auto tickCount                = xTaskGetTickCount() - start;
+            auto writeSpeedInBytesPerMsec = writeSize / TICKS_TO_MSEC(tickCount);
+            LOG_DEBUG("MMC write speed %ld B/ms", (long)(writeSpeedInBytesPerMsec));
+
+            bufferOperation(BufOp::Clear, workBuf.get(), writeSize);
+            start = xTaskGetTickCount();
+            diskManager->read(handle, workBuf.get(), firstTestSector, testSizeInSectors);
+            tickCount                    = xTaskGetTickCount() - start;
+            auto readSpeedInBytesPerMsec = writeSize / TICKS_TO_MSEC(tickCount);
+            LOG_DEBUG("MMC read speed %ld B/ms", (long)(readSpeedInBytesPerMsec));
+
+            bufferOperation(BufOp::Compare, workBuf.get(), writeSize);
+            if (bufferOperation(BufOp::Compare, workBuf.get(), writeSize) == false) {
+                LOG_ERROR("Bad write on MMC");
+            }
+
+            return true;
+        }
+
         auto ret = true;
         ret &=
             sys::SystemManager::CreateService(std::make_shared<EventManager>(service::name::evt_manager), sysmgr.get());
