0ff0af7f34f93ba1d67e438160d5d62855f8f1c5
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2021-02-26 15:15:36 +0100

    add clang target

---
+++ Target_Linux_clang.cmake
@@ -0,0 +1,35 @@
+set(PROJECT_TARGET "TARGET_Linux" CACHE INTERNAL "")
+
+set(TARGET_SOURCES CACHE INTERNAL "")
+
+set(TARGET_DIR_INCLUDES ${CMAKE_CURRENT_LIST_DIR}/board/linux  CACHE INTERNAL "")
+
+set(TARGET_COMPILE_FEATURES CACHE INTERNAL "")
+
+set(TARGET_COMPILE_DEFINITIONS CACHE INTERNAL "")
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+
+
+add_compile_options(-funsigned-char -Wno-unknown-warning-option)
+
+set(TARGET_LIBRARIES
+    rt
+    pthread
+    portaudio
+    CACHE INTERNAL "" )
+
+
+option (LINUX_ENABLE_SANITIZER "Enable address sanitizer for Linux" OFF)
+if (LINUX_ENABLE_SANITIZER)
+    add_compile_options(-fsanitize=address -static-libasan)
+    add_link_options(-fsanitize=address -static-libasan)
+endif (LINUX_ENABLE_SANITIZER)
+
+set(CMAKE_STRIP strip CACHE INTERNAL "")
+set(CMAKE_OBJCOPY objcopy CACHE INTERNAL "")
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/sys)
+file(CREATE_LINK ${CMAKE_BINARY_DIR}/user ${CMAKE_BINARY_DIR}/sys/user SYMBOLIC)
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/sys/current)
+file(CREATE_LINK ${CMAKE_BINARY_DIR}/assets ${CMAKE_BINARY_DIR}/sys/current/assets SYMBOLIC)
+
