43af085d0e97cf489fb01b4c351261dee579fa13
Konrad Janowski <konrad.janowski@mudita.com>
2021-03-01 16:33:13 +0100

    add test call

---
+++ QA/tests/test_call.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+# For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+import time
+import pytest
+
+from tools.harness.interface.defs import key_codes
+from tools.utils import get_calllog_count
+
+
+@pytest.mark.rt1051
+@pytest.mark.usefixtures("phone_unlocked")
+def test_call(harness, phone_number, call_duration):
+    count_before = get_calllog_count(harness)
+    # enter number
+    harness.send_number(str(phone_number))
+    # call
+    harness.connection.send_key_code(key_codes["fnLeft"])
+    time.sleep(call_duration)
+    # hang up
+    harness.connection.send_key_code(key_codes["fnRight"])
+    count_after = get_calllog_count(harness)
+
+    assert count_before + 1 == count_after
+    time.sleep(2)  # needed to restore after call
