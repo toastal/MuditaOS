67bfa14409ca45e362ec64142d906a6a87bd18b0
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2021-07-29 00:33:38 +0200

    empty dir test

--- module-apps/application-music-player/tests/unittest_songrepository.cpp
+++ module-apps/application-music-player/tests/unittest_songrepository.cpp
@@ -6,23 +6,63 @@
 
 #include <SongsRepository.hpp>
 
+#include <filesystem>
+#include <stdexcept>
+
+namespace fs = std::filesystem;
+
+constexpr auto testDir  = "appmusic-test";
+constexpr auto emptyDir = "empty";
+auto testDirPath        = fs::path(testDir);
 class MockTagsFetcher : public ::app::music_player::AbstractTagsFetcher
 {
   public:
     MOCK_METHOD(std::optional<audio::Tags>, getFileTags, (const std::string &filePath), (override));
 };
 
-auto getMockedRepository()
+class SongsRepositoryFixture : public ::testing::Test
 {
-    return std::make_unique<app::music_player::SongsRepository>(std::make_unique<MockTagsFetcher>());
-}
+  protected:
+    static void SetUpTestSuite()
+    {
+        if (fs::exists(testDirPath)) {
+            TearDownTestSuite();
+        }
 
-TEST(SongsRepository, LazyInit)
+        fs::create_directory(testDirPath);
+        fs::create_directory(testDirPath / emptyDir);
+    }
+
+    static void TearDownTestSuite()
+    {
+        fs::remove_all(testDir);
+    }
+
+    auto getMockedRepository(std::string directoryToScan)
+    {
+        return std::make_unique<app::music_player::SongsRepository>(std::make_unique<MockTagsFetcher>(),
+                                                                    directoryToScan);
+    }
+};
+
+TEST_F(SongsRepositoryFixture, LazyInit)
 {
-    auto repo       = getMockedRepository();
+    auto repo       = getMockedRepository(testDir);
     auto musicFiles = repo->getMusicFilesList();
     EXPECT_EQ(musicFiles.size(), 0);
 }
+
+TEST_F(SongsRepositoryFixture, Empty)
+{
+    auto repo = getMockedRepository(testDirPath / emptyDir);
+    repo->scanMusicFilesList();
+
+    auto musicFiles = repo->getMusicFilesList();
+    EXPECT_EQ(musicFiles.size(), 0);
+}
+
+TEST_F(SongsRepositoryFixture, Scan)
+{}
 
 int main(int argc, char **argv)
 {
