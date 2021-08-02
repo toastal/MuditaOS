b065ae44143960fae4b8db42fff9fd01a4bc2b8b
Piotr Tański <piotr.tanski@mudita.com>
2021-08-02 17:45:31 +0200

    wip 2

--- products/BellHybrid/app-window/AppWindow.cpp
+++ products/BellHybrid/app-window/AppWindow.cpp
@@ -21,12 +21,119 @@ namespace gui
         setSize(style::window_width, style::window_height);
     }
 
-    void AppWindow::buildInterface()
+    void AppWindow::destroyInterface()
+    {
+        erase(statusBar);
+        erase(header);
+        erase(bottomBar);
+        statusBar = nullptr;
+        header    = nullptr;
+        bottomBar = nullptr;
+    }
+
+    void AppWindow::rebuild()
     {}
+
+    void AppWindow::buildInterface()
+    {
+        auto config          = configureStatusBar(application->getStatusBarConfiguration());
+        namespace status_bar = style::status_bar;
+        statusBar            = new gui::status_bar::StatusBar(this,
+                                                   status_bar::default_horizontal_pos,
+                                                   status_bar::default_vertical_pos,
+                                                   status_bar::width,
+                                                   status_bar::height);
+        statusBar->configure(std::move(config));
+
+        header = new gui::header::Header(this,
+                                         style::header::default_horizontal_pos,
+                                         style::header::default_vertical_pos,
+                                         style::header::width,
+                                         style::header::height);
+
+        bottomBar = new gui::BottomBar(this, 0, style::window_height - 51, style::window_width, 50);
+        bottomBar->setActive(BottomBar::Side::LEFT, false);
+        bottomBar->setActive(BottomBar::Side::CENTER, false);
+        bottomBar->setActive(BottomBar::Side::RIGHT, false);
+    }
+
+    status_bar::Configuration AppWindow::configureStatusBar(status_bar::Configuration appConfiguration)
+    {
+        return appConfiguration;
+    }
+
+    void AppWindow::applyToStatusBar(StatusBarConfigurationChangeFunction configChange)
+    {
+        if (configChange) {
+            auto newConfiguration = configChange(statusBar->getConfiguration());
+            statusBar->configure(std::move(newConfiguration));
+        }
+    }
+
+    bool AppWindow::updateSim()
+    {
+        if (statusBar == nullptr) {
+            return false;
+        }
+        return statusBar->updateSim();
+    }
+
+    bool AppWindow::updateBatteryStatus()
+    {
+        if (statusBar == nullptr) {
+            return false;
+        }
+        return statusBar->updateBattery();
+    }
+    // updates battery level in the window
+    bool AppWindow::updateSignalStrength()
+    {
+        if (statusBar == nullptr) {
+            return false;
+        }
+        return statusBar->updateSignalStrength();
+    }
+
+    bool AppWindow::updateNetworkAccessTechnology()
+    {
+        if (statusBar == nullptr) {
+            return false;
+        }
+        return statusBar->updateNetworkAccessTechnology();
+    }
+
+    void AppWindow::updatePhoneMode(sys::phone_modes::PhoneMode mode)
+    {
+        auto fn = [&](gui::status_bar::Configuration cfg) -> gui::status_bar::Configuration {
+            gui::status_bar::Configuration ret(cfg);
+            ret.setPhoneMode(mode);
+            return ret;
+        };
+
+        applyToStatusBar(std::move(fn));
+    }
+
+    bool AppWindow::preventsAutoLocking() const noexcept
+    {
+        return preventsAutoLock;
+    }
 
     bool AppWindow::updateTime()
     {
-        return true;
+        if (statusBar == nullptr) {
+            return false;
+        }
+        return statusBar->updateTime();
+    }
+
+    void AppWindow::setTitle(const UTF8 &text)
+    {
+        header->setTitle(text);
+    }
+
+    UTF8 AppWindow::getTitle()
+    {
+        return header->getTitle();
     }
 
     bool AppWindow::onDatabaseMessage(sys::Message *msg)
@@ -85,6 +192,82 @@ namespace gui
         }
 
         return false;
+    }
+
+    void AppWindow::bottomBarTemporaryMode(const UTF8 &text, bool emptyOthers)
+    {
+        bottomBarTemporaryMode(text, BottomBar::Side::LEFT, emptyOthers);
+    }
+
+    void AppWindow::bottomBarTemporaryMode(const UTF8 &text, BottomBar::Side side, bool emptyOthers)
+    {
+        if (bottomBar == nullptr) {
+            return;
+        }
+
+        bottomBar->store();
+
+        if (emptyOthers) {
+            bottomBar->setText(BottomBar::Side::LEFT, "");
+            bottomBar->setText(BottomBar::Side::CENTER, "");
+            bottomBar->setText(BottomBar::Side::RIGHT, "");
+        }
+
+        switch (side) {
+        case BottomBar::Side::LEFT:
+            bottomBar->setText(BottomBar::Side::LEFT, text);
+            break;
+        case BottomBar::Side::CENTER:
+            bottomBar->setText(BottomBar::Side::CENTER, text);
+            break;
+        case BottomBar::Side::RIGHT:
+            bottomBar->setText(BottomBar::Side::RIGHT, text);
+            break;
+        }
+        application->refreshWindow(gui::RefreshModes::GUI_REFRESH_FAST);
+    }
+
+    void AppWindow::bottomBarRestoreFromTemporaryMode()
+    {
+        if (bottomBar == nullptr) {
+            return;
+        }
+
+        bottomBar->restore();
+        application->refreshWindow(gui::RefreshModes::GUI_REFRESH_FAST);
+    }
+
+    void AppWindow::setBottomBarText(const UTF8 &text, BottomBar::Side side)
+    {
+        bottomBar->setText(side, text);
+    }
+
+    void AppWindow::clearBottomBarText(BottomBar::Side side)
+    {
+        bottomBar->setText(side, "");
+    }
+
+    bool AppWindow::selectSpecialCharacter()
+    {
+        application->clearLongPressTimeout();
+        return app::manager::Controller::sendAction(
+            application,
+            app::manager::actions::ShowSpecialInput,
+            std::make_unique<gui::SwitchSpecialChar>(gui::SwitchSpecialChar::Type::Request, application->GetName()),
+            app::manager::OnSwitchBehaviour::RunInBackground);
+    }
+
+    BoundingBox AppWindow::bodySize()
+    {
+        return {0,
+                header->offset_h(),
+                this->getWidth(),
+                this->getHeight() - this->header->offset_h() - bottomBar->getHeight()};
+    }
+
+    void AppWindow::setBottomBarActive(BottomBar::Side side, bool value)
+    {
+        bottomBar->setActive(side, value);
     }
 
     void AppWindow::accept(GuiVisitor &visitor)
--- products/BellHybrid/app-window/include/AppWindow.hpp
+++ products/BellHybrid/app-window/include/AppWindow.hpp
@@ -32,7 +32,33 @@ namespace gui
     class AppWindow : public Window
     {
       protected:
+        /**
+         * Information bar for signal, battery and lock icon on the top of the screen.
+         */
+        gui::status_bar::StatusBar *statusBar = nullptr;
+        /**
+         * Information bar for window title and additional action buttons.
+         */
+        gui::header::Header *header = nullptr;
+        /**
+         * Information bar for the buttons on the bottom of the page.
+         */
+        gui::BottomBar *bottomBar = nullptr;
+        /**
+         * Pointer to the application object that owns the window.
+         */
         app::Application *application = nullptr;
+
+        /**
+         * A function that applies configuration changes to the current status bar configuration.
+         */
+        using StatusBarConfigurationChangeFunction =
+            std::function<status_bar::Configuration(status_bar::Configuration)>;
+
+        /**
+         * A flag that is set if current window state requires the phone to stay unlocked
+         */
+        bool preventsAutoLock = false;
 
       public:
         AppWindow() = delete;
@@ -46,11 +72,52 @@ namespace gui
         std::string getUniqueName() override;
 
         virtual bool onDatabaseMessage(sys::Message *msg);
+
+        bool updateSim();
+        bool updateBatteryStatus();
+        bool updateSignalStrength();
+        bool updateNetworkAccessTechnology();
+        void updatePhoneMode(sys::phone_modes::PhoneMode mode);
+        [[nodiscard]] bool preventsAutoLocking() const noexcept;
         virtual bool updateTime();
 
+        void rebuild() override;
         void buildInterface() override;
+        void destroyInterface() override;
         bool onInput(const InputEvent &inputEvent) override;
         void accept(GuiVisitor &visitor) override;
+
+        /**
+         * Configure the status bar using window-specific configuration.
+         * @param appConfiguration      Application-wide status bar configuration that it to be modified.
+         * @return window-specific configuration of the status bar.
+         */
+        virtual status_bar::Configuration configureStatusBar(status_bar::Configuration appConfiguration);
+
+        /**
+         * Applies configuration change on the current status bar configuration.
+         * @param configChange  The function that contains the status bar configuration changes.
+         */
+        void applyToStatusBar(StatusBarConfigurationChangeFunction configChange);
+
+        void setTitle(const UTF8 &text);
+        [[nodiscard]] UTF8 getTitle();
+
+        /// Setting bottom bar temporary text
+        /// @param text - bottomBar text
+        /// @param overwriteOthers - set or not other bottomBar texts to "" (default true)
+        void bottomBarTemporaryMode(const UTF8 &text, bool emptyOthers = true);
+        void bottomBarTemporaryMode(const UTF8 &text, BottomBar::Side side, bool emptyOthers = true);
+        void bottomBarRestoreFromTemporaryMode();
+        void setBottomBarText(const UTF8 &text, BottomBar::Side side);
+        void clearBottomBarText(BottomBar::Side side);
+        bool selectSpecialCharacter();
+        void setBottomBarActive(BottomBar::Side side, bool value);
+
+        /// get BoundingBox size of Window "body" area
+        /// @note it would be much better to just have "body item" instead
+        /// but it would mean not insignificant refactor
+        BoundingBox bodySize();
     };
 
 } /* namespace gui */
