917d11bfc931b48637ff600ea5081f932ccb8681
Przemyslaw Brudny <przemyslaw.brudny@mudita.com>
2021-08-17 12:08:40 +0200

    Wiiiiip

---
+++ products/BellHybrid/app-window/AppWindow.cpp
@@ -0,0 +1,101 @@
+﻿// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include <AppWindow.hpp>
+#include "Application.hpp"
+#include "InputEvent.hpp"
+#include "StatusBar.hpp"
+#include "status-bar/Time.hpp"
+#include <Style.hpp>
+#include <i18n/i18n.hpp>
+#include <service-appmgr/Controller.hpp>
+#include <service-audio/AudioServiceAPI.hpp>
+
+using namespace style::header;
+
+namespace gui
+{
+
+    AppWindow::AppWindow(app::Application *app, std::string name) : Window(name), application{app}
+    {
+        setSize(style::window_width, style::window_height);
+    }
+
+    void AppWindow::buildInterface()
+    {}
+
+    bool AppWindow::updateTime()
+    {
+        return true;
+    }
+
+    bool AppWindow::onDatabaseMessage(sys::Message *msg)
+    {
+        return false;
+    }
+
+    bool AppWindow::onInput(const InputEvent &inputEvent)
+    {
+        // check if any of the lower inheritance onInput methods catch the event
+        if (Window::onInput(inputEvent)) {
+            return true;
+        }
+
+        if (inputEvent.isLongRelease(gui::KeyCode::KEY_RF)) {
+            LOG_INFO("exit to main menu");
+            app::manager::Controller::sendAction(application, app::manager::actions::Home);
+        }
+
+        if (inputEvent.isLongRelease(gui::KeyCode::KEY_PND)) {
+            LOG_INFO("Locking phone");
+
+            application->getPhoneLockSubject().lock();
+        }
+
+        if ((inputEvent.isShortRelease())) {
+            switch (inputEvent.getKeyCode()) {
+            case KeyCode::HEADSET_VOLUP:
+                [[fallthrough]];
+            case KeyCode::KEY_VOLUP: {
+                return application->increaseCurrentVolume();
+            }
+            case KeyCode::HEADSET_VOLDN:
+                [[fallthrough]];
+            case KeyCode::KEY_VOLDN: {
+                return application->decreaseCurrentVolume();
+            }
+            case KeyCode::KEY_RF: {
+                application->returnToPreviousWindow();
+                return true;
+            }
+            default:
+                break;
+            }
+        }
+
+        if (inputEvent.is(KeyCode::KEY_TORCH)) {
+            if (inputEvent.isLongRelease()) {
+                application->toggleTorchOnOff();
+                return true;
+            }
+            else if (inputEvent.isShortRelease()) {
+                application->toggleTorchColor();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void AppWindow::accept(GuiVisitor &visitor)
+    {
+        visitor.visit(*this);
+    }
+
+    std::string AppWindow::getUniqueName()
+    {
+        constexpr auto separator = "/";
+        return application->GetName() + separator + getName();
+    }
+
+} /* namespace gui */
---
+++ products/BellHybrid/app-window/include/AppWindow.hpp
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <gui/widgets/StatusBar.hpp>
+#include <gui/widgets/header/Header.hpp>
+#include <gui/widgets/BottomBar.hpp>
+#include <gui/widgets/Window.hpp>
+#include <Service/Service.hpp>
+#include <Service/Message.hpp>
+
+namespace app
+{
+    class Application;
+};
+
+namespace gui
+{
+    namespace name
+    {
+        namespace window
+        {
+            inline constexpr auto main_window = "MainWindow";
+            inline constexpr auto no_window   = "";
+        } // namespace window
+    }     // namespace name
+
+    /*
+     * @brief This is wrapper for gui window used within applications.
+     */
+    class AppWindow : public Window
+    {
+      protected:
+        app::Application *application = nullptr;
+
+      public:
+        AppWindow() = delete;
+        AppWindow(app::Application *app, std::string name);
+
+        app::Application *getApplication()
+        {
+            return application;
+        };
+
+        std::string getUniqueName() override;
+
+        virtual bool onDatabaseMessage(sys::Message *msg);
+        virtual bool updateTime();
+
+        void buildInterface() override;
+        bool onInput(const InputEvent &inputEvent) override;
+        void accept(GuiVisitor &visitor) override;
+    };
+
+} /* namespace gui */
