5e85341c876c077a998abdc1f3d8f66b18a8e69d
mkamonMdt <michal.kamon@mudita.com>
2021-08-28 19:26:19 +0200

    [BH-827] PowerNap main window implementation.
    The commit covers:
    *  Window, Model, List Item classes implementation.
    *  Loading and saving data from/to database.

--- products/BellHybrid/apps/application-bell-powernap/windows/PowerNapMainWindow.hpp
+++ products/BellHybrid/apps/application-bell-powernap/windows/PowerNapMainWindow.hpp
@@ -1,15 +1,25 @@
 // Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
+// Copyright (c) 2018-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
 #pragma once
 
 #include "presenter/PowerNapMainWindowPresenter.hpp"
 #include <AppWindow.hpp>
+
 namespace gui
 {
+    class SideListView;
     class PowerNapMainWindow : public AppWindow, public app::powernap::PowerNapMainWindowContract::View
     {
         std::unique_ptr<app::powernap::PowerNapMainWindowContract::Presenter> windowPresenter;
+        SideListView *sideListView;
+
+        bool onInput(const gui::InputEvent &inputEvent) override;
+        void buildInterface() override;
+        status_bar::Configuration configureStatusBar(status_bar::Configuration appConfiguration) override;
 
       public:
         PowerNapMainWindow(app::Application *app,
